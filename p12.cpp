2c726cefaf22361b3b0388cfa0507340ce4191c8
Srivathsa Anandgal <srivathsaanandgal2k1@gmail.com>
2025-01-03 20:19:17 +0530

    p12 : Simple Calculator

--- p12.cpp
+++ p12.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 int main()
 {
-    cout<<"Calculator Program"<<endl;
-    cout<<"Calculator Program"<<endl;
+    cout<<"Welcome to the Calculator Program "<<endl;
+    double a , b;
+    char op;
+    cout<<"Enter the you want to carry out the operation on : "<<endl;
+    cin>>a>>b;
+    cout<<"Enter a for addition , s for subtraction , m for multiplication and d for division . Case Sensitive"<<endl;
+    cin>>op;
+    switch (op)
+    {
+    case 'a' :
+        cout<<"The result is : "<<a+b<<endl;
+        break;
+    case 's' :
+        cout<<"The result is : "<<a-b<<endl;
+        break;
+    case 'm' :
+        cout<<"The result is : "<<a*b<<endl;
+        break;
+    case 'd' :
+        cout<<"The result is : "<<a/b<<endl;
+        break;
+    default:
+        cout<<"Please try again !!"<<endl;
+        break;
+    }
+    cout<<"Calculator Program Ends !!!"<<endl;
     return 0;
 }
